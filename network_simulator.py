f23a1207e123bfd08c1d0662c11aa8dc03debaae
Brian Tokumoto <bht8183@g.rit.edu>
2025-03-24 19:17:49 -0400

    I think fcuntionality wise it is done

--- network_simulator.py
+++ network_simulator.py
@@ -32,6 +32,7 @@ def simulator(listen_ip, listen_port, forward_ip, forward_port,
 
     def handle_incoming_from_A():
         while True:
+            
             data, addr = sock_in.recvfrom(4096)
             print("Simulator received", len(data), "bytes from", addr)
             # Possibly drop
